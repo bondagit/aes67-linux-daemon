f8b535252a001330b2d5704f0c447493e3d7381a
Andrea Bondavalli <andrea.bondavalli74@gmail.com>
2020-02-01 10:03:03 -0800

    Fixes to cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,14 +4,14 @@ rm -rf 3rdparty/ravenna-alsa-lkm
 rm -rf 3rdparty/cpp-httplib
 
 rm -f daemon/status.json
-rm -f daemon/CMakeFiles
+rm -rf daemon/CMakeFiles
 rm -f daemon/Makefile
 rm -f daemon/CMakeCache.txt
 rm -f daemon/cmake_install.cmake
 rm -f daemon/CTestTestfile.cmake
 rm -f daemon/Testing
 rm -f daemon/aes67-daemon
-rm -f daemon/tests/CMakeFiles
+rm -rf daemon/tests/CMakeFiles
 rm -f daemon/tests/Makefile
 rm -f daemon/tests/CMakeCache.txt
 rm -f daemon/tests/cmake_install.cmake
@@ -21,8 +21,8 @@ rm -f daemon/tests/daemon-test
 
 rm -f demo/sink-test.wav
 
-rm -f webui/build
-rm -f webui/node_modules
+rm -rf webui/build
+rm -rf webui/node_modules
 rm -f webui/*.log
 rm -f webui/package-lock.json
 
