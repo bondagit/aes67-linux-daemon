c8a3f300526c14042e9fd76ad468dc31f6b4b11e
Stephan <github@flycam.org>
2022-06-25 14:23:37 +0200

    Simplified and speed up package installation
    Simplified the package installation script by installing all packages in one apt command instead of many idividual ones. 
    This speeds up installation on slower systems (Pi) significantly.

--- ubuntu-packages.sh
+++ ubuntu-packages.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 #
-# Tested on Ubuntu 18.04
+# Tested on Ubuntu 22.04 LTS
 #
 
 sudo apt update
-sudo apt-get install -y psmisc
-sudo apt-get install -y build-essential
-sudo apt-get install -y clang
-sudo apt-get install -y git
-sudo apt-get install -y cmake
-# sudo apt-get install -y npm
-sudo apt-get install -y libboost-all-dev
-sudo apt-get install -y valgrind
-sudo apt-get install -y linux-sound-base
-sudo apt-get install -y alsa-base
-sudo apt-get install -y alsa-utils
-sudo apt-get install -y libasound2-dev
-sudo apt-get install -y linuxptp
-sudo apt-get install -y libavahi-client-dev
-sudo apt install -y linux-headers-$(uname -r)
-
+sudo apt -y install psmisc build-essential clang git cmake libboost-all-dev valgrind linux-sound-base alsa-base alsa-utils libasound2-dev linuxptp libavahi-client-dev linux-headers-$(uname -r)
