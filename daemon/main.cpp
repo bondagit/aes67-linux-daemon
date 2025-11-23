96a006f7bb9fa921d97b75b3a59d291575070950
Andrea Bondavalli <andrea.bondavalli74@gmail.com>
2025-11-23 18:12:53 +0100

    Added HTTP Live Streaming of the daemon's sinks compatible with VideoLAN using AAC LC encoder

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -43,7 +43,7 @@ namespace po = boost::program_options;
 namespace postyle = boost::program_options::command_line_style;
 namespace logging = boost::log;
 
-static const std::string version("bondagit-2.1.2");
+static const std::string version("bondagit-2.2.0");
 static std::atomic<bool> terminate = false;
 
 void termination_handler(int signum) {
